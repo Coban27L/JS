2e348c848f05056b9901c5c81333eb052758c2a1
Coban27L <zo7681@gmail.com>
2024-09-25 23:18:55 +0800

    25/09/24
    login

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function handleLoginBtnClick() {
-    const username = loginInput.value ;
-    console.log(username);
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick)
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
